302278d09c6888c318c7bf3c2497101df99edd3b
Nick Genovese <ngenovese@pepperdash.com>
2020-06-11 11:20:17 -0400

    initial solution setup

---
+++ Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Crestron.SimplSharp;                          				// For Basic SIMPL# Classes
+using Crestron.SimplSharpPro;                       				// For Basic SIMPL#Pro classes
+
+namespace AxisCameraEpi
+{
+    public class Class1
+    {
+    }
+}
+
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("AxisCameraEpi")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("AxisCameraEpi")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2020")]
+[assembly: AssemblyVersion("1.0.0.*")]
+
